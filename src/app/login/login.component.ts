a6e1678ce006bd0ce7ab4016a26057f919f6b158
Valentino Strebel <tino_schweizer@hotmail.com>
2025-06-06 14:35:16 +0200

    inserted prototype of google login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,23 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { LoginService } from '../services/login.service';
+
+declare const google: any;
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
-
-
-
-    constructor(private loginService: LoginService, private router: Router) {}
+export class LoginComponent implements OnInit {
+  constructor(
+    private loginService: LoginService,
+    private router: Router,
+    private ngZone: NgZone
+  ) {}
 
   doLogin() {
-    this.loginService.login();             // Login setzen
-    this.router.navigate(['/dashboard']);  // Weiterleitung
+    this.loginService.login(); // Login setzen
+    this.router.navigate(['/dashboard']); // Weiterleitung
+  }
+
+  ngOnInit(): void {
+    google.accounts.id.initialize({
+      client_id:
+        '996939012758-t5ckq9m6kv89o8354soli6tavt23k1ho.apps.googleusercontent.com',
+      callback: this.handleCredentialResponse.bind(this),
+    });
+
+    google.accounts.id.renderButton(document.getElementById('googleBtn'), {
+      theme: 'outline',
+      size: 'large',
+    });
+
+    google.accounts.id.prompt();
+  }
+
+  handleCredentialResponse(response: any) {
+    const token = response.credential;
+    console.log('Received JWT:', token);
+
+    localStorage.setItem('google_token', token);
+
+    this.ngZone.run(() => {
+      this.router.navigate(['/dashboard']);
+    });
   }
 }
