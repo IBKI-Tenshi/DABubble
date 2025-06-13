8456a723959bae50cd6018265abbc7ff12f9e270
Valentino Strebel <tino_schweizer@hotmail.com>
2025-06-13 07:01:25 +0200

    first draft avatar window

--- src/app/create-account/avatar-selection/avatar-selection.component.ts
+++ src/app/create-account/avatar-selection/avatar-selection.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
 
 @Component({
   selector: 'app-avatar-selection',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './avatar-selection.component.html',
-  styleUrl: './avatar-selection.component.scss'
+  styleUrl: './avatar-selection.component.scss',
 })
 export class AvatarSelectionComponent {
+  constructor(private router: Router) {}
 
+  successMove() {
+    this.router.navigate(['/']);
+  }
 }
