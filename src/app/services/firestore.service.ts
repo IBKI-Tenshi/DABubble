72e7df17336d5d30771d641f64a791278b099efd
IBKI-Tenshi <kitak.borna@gmail.com>
2025-06-19 12:38:25 +0200

    quick save

--- src/app/services/firestore.service.ts
+++ src/app/services/firestore.service.ts
@@ -1,68 +1,5 @@
 
 
-// import { Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { UrlService } from './url.service';
-// import { Observable } from 'rxjs';
-// import { Message } from '../../models/message.model';
-// import {
-//   Firestore,
-//   doc,
-//   getDoc,
-//   setDoc,
-//   collection,
-//   addDoc,
-//   collectionData,
-//   DocumentData,
-// } from '@angular/fire/firestore';
-
-// @Injectable({
-//   providedIn: 'root',
-// })
-// export class FirestoreService {
-//   constructor(
-//     private http: HttpClient,
-//     private urlService: UrlService,
-//     private firestore: Firestore
-//   ) {}
-
-//   // REST: Nutzer (nur hier sinnvoll)
-//   getAllUsers(): Observable<any> {
-//     const url = `${this.urlService.BASE_URL}/users`;
-//     return this.http.get(url);
-//   }
-
-//   // Firestore: Chat-Dokument lesen
-//   async getChatById(chatId: string): Promise<any> {
-//     const chatDocRef = doc(this.firestore, 'chats', chatId);
-//     const snapshot = await getDoc(chatDocRef);
-//     return snapshot;
-//   }
-
-//   // Firestore: Chat-Dokument erstellen
-//   createChat(chatId: string, participants: string[]): Promise<void> {
-//     const chatDocRef = doc(this.firestore, 'chats', chatId);
-//     return setDoc(chatDocRef, {
-//       participants,
-//       createdAt: new Date().toISOString()
-//     });
-//   }
-
-//   // Firestore: Nachrichten auslesen (Subcollection)
-//   getChatMessages(chatId: string): Observable<Message[]> {
-//     const messagesCollection = collection(this.firestore, 'chats', chatId, 'messages');
-//     return collectionData(messagesCollection, { idField: 'id' }) as Observable<Message[]>;
-//   }
-
-//   // Firestore: Neue Nachricht hinzufügen
-//   addMessageToChat(chatId: string, message: Message): Promise<void> {
-//     const messagesCollection = collection(this.firestore, 'chats', chatId, 'messages');
-//     return addDoc(messagesCollection, message).then(() => {});
-//   }
-// }
-
-
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { UrlService } from './url.service';
@@ -80,6 +17,11 @@ import {
   orderBy
 } from '@angular/fire/firestore';
 
+import { Timestamp } from 'firebase/firestore';
+import { map } from 'rxjs/operators';
+
+
+
 @Injectable({
   providedIn: 'root',
 })
@@ -88,7 +30,7 @@ export class FirestoreService {
     private http: HttpClient,
     private urlService: UrlService,
     private firestore: Firestore
-  ) {}
+  ) { }
 
   // REST: Nutzer (nur hier sinnvoll)
   getAllUsers(): Observable<any> {
@@ -100,7 +42,7 @@ export class FirestoreService {
   async getChatById(chatId: string): Promise<any> {
     const chatDocRef = doc(this.firestore, 'chats', chatId);
     console.log(chatId);
-    
+
     const snapshot = await getDoc(chatDocRef);
     return snapshot;
   }
@@ -117,15 +59,25 @@ export class FirestoreService {
   //  Firestore: Nachrichten auslesen (Subcollection mit Live-Update)
   getChatMessages(chatId: string): Observable<Message[]> {
     const messagesCollection = collection(this.firestore, 'chats', chatId, 'messages');
-    console.log('Collection reference:', messagesCollection);
     const messagesQuery = query(messagesCollection, orderBy('timestamp', 'asc'));
-    console.log("load message test2");
-    return collectionData(messagesQuery, { idField: 'id' }) as Observable<Message[]>;
+    // return collectionData(messagesQuery, { idField: 'id' }) as Observable<Message[]>;
+    return collectionData(messagesQuery) as Observable<Message[]>;
+
   }
+
+  //   getChatMessages(chatId: string): Observable<Message[]> {
+  //   const messagesCollection = collection(this.firestore, 'chats', chatId, 'messages');
+  //   // Kein orderBy hier!
+  //   return collectionData(messagesCollection, { idField: 'id' }) as Observable<Message[]>;
+  // }
+
 
   // Firestore: Neue Nachricht hinzufügen
   addMessageToChat(chatId: string, message: Message): Promise<void> {
     const messagesCollection = collection(this.firestore, 'chats', chatId, 'messages');
-    return addDoc(messagesCollection, message).then(() => {});
+    return addDoc(messagesCollection, message).then(() => { });
   }
+
+
+
 }
--- src/models/message.model.ts
+++ src/models/message.model.ts
@@ -1,5 +1,8 @@
+import { Timestamp } from 'firebase/firestore';
+
 export interface Message {
   text: string;
   senderId: string;
-  timestamp: string;
+  // timestamp: Timestamp;
+    timestamp: string;
 }
